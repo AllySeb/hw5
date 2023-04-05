721993100322cb7980ecde808c9ae4dc16c046aa
Allyson Seba <aseba@usc.edu>
2023-04-05 13:50:16 +0000

    finished hw5

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,6 +20,16 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+void scheduler(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+		const size_t w_cnt,
+    size_t n_day,
+    size_t d_needed,
+    std::vector<size_t> shiftCnt,
+		std::vector<size_t> shiftToday,
+    DailySchedule& sched,
+		bool& found);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -31,14 +41,88 @@ bool schedule(
     DailySchedule& sched
 )
 {
-    if(avail.size() == 0U){
+    if(avail.size() == 0){
         return false;
     }
     sched.clear();
     // Add your code below
 
+		// # of workers
+		const int w_cnt = avail[0].size();
+
+		// keeps track of # of shifts per worker, starts a 0 
+		std::vector<size_t> shiftCnt(w_cnt, 0);
+		std::vector<size_t> shiftToday(w_cnt, 0);
+
+		// setting up schedule
+		// note: avail.size() is the # of days
+		for (size_t n = 0; n < avail.size(); n++){
+			std::vector<Worker_T> temp;
+			for (size_t d = 0; d < dailyNeed; d++){
+				temp.push_back(INVALID_ID);
+			}
+			sched.push_back(temp);
+		}
+
+		bool found = false;
+
+		scheduler(avail, dailyNeed, maxShifts, w_cnt, 0, 0, shiftCnt, shiftToday, sched, found);
+		return found;
+
+}
 
 
+void scheduler(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+		const size_t w_cnt,
+    size_t n_day,
+    size_t d_needed,
+    std::vector<size_t> shiftCnt,
+		std::vector<size_t> shiftToday,
+    DailySchedule& sched,
+		bool& found)
+{
+    // iterations should deal with scheduling a day at a time
+		if (found){
+			return;
+		}
+    // if the day is unscheduled and the # of needed workers (d) not reached
+		else {
+	    // for each worker that exists
+      // (avail[0] gives the first column, each columns is a different worker)
+			// k = worker
+      for (unsigned int k = 0; k < w_cnt; k++){
+				if (found || sched[avail.size() - 1][dailyNeed - 1] != INVALID_ID) {
+					return;
+				}
+        if (avail[n_day][k] == 1 && shiftCnt[k] < maxShifts && shiftToday[k] == 0){
+					// insert worker id into that time slot
+					sched[n_day][d_needed] = k;
+					// increase their shift tracker by 1
+					shiftCnt[k] += 1;
+					// track that they are already scheduled for today as well
+					shiftToday[k] += 1;
 
+					// EARLY RETURN CASE:
+					if (sched[avail.size() - 1][dailyNeed - 1] != INVALID_ID) {
+						found = true;
+						return;
+					}
+
+					// when at last col, recurse to next row in array
+					if (d_needed == dailyNeed - 1){ // next row, reset shiftToday
+						std::vector<size_t> shiftToday(w_cnt, 0);
+						scheduler(avail, dailyNeed, maxShifts, w_cnt, n_day + 1, 0, shiftCnt, shiftToday, sched, found);
+					}
+					else{ // next col
+						scheduler(avail, dailyNeed, maxShifts, w_cnt, n_day, d_needed + 1, shiftCnt, shiftToday, sched, found);
+					}
+
+					shiftCnt[k] -= 1;
+					shiftToday[k] -= 1;
+				} 
+      }
+		}
 }
 
--- wordle.cpp
+++ wordle.cpp
@@ -5,16 +5,23 @@
 #include <algorithm> 
 #include <map>
 #include <set>
+#include <vector>
 #endif
 
 #include "wordle.h"
 #include "dict-eng.h"
 using namespace std;
 
+void helper(std::set<std::string>& words, 
+  const std::set<std::string>& dict, 
+  const std::string& original, 
+  std::string str, 
+  std::vector<char> floating, 
+  unsigned int idx);
+
+int dashCounter(const std::string& original, std::string str);
 
 // Add prototypes of helper functions here
-
-
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -23,6 +30,89 @@ std::set<std::string> wordle(
 {
     // Add your code here
 
+    std::set<std::string> words;
+    std::string str = "";
+
+		std::vector<char> sfloating;
+    for (unsigned int i = 0; i < floating.size(); i++){
+      sfloating.push_back(floating[i]);
+		}
+    
+		helper(words, dict, in, str, sfloating, 0);
+
+		return words;
+
 }
 
 // Define any helper functions here
+
+
+// void helper func 
+// (params: refence to set, string, alphabet, index in string)
+void helper(std::set<std::string>& words, 
+  const std::set<std::string>& dict, 
+  const std::string& original, 
+  std::string str, 
+  std::vector<char> floating, 
+  unsigned int idx)
+{
+// base case: if at the end of in word
+	if (idx == original.size()){
+		// check if in dictionary
+		// if in dict, add to set, and return nothing!
+		if (dict.find(str) != dict.end()){
+			words.insert(str);
+		}
+		return;
+	}
+  if (original[idx] == '-'){
+    // if "-" at that index, change
+    // first, handle floating chars
+    // if number of dashes < the number of floating, early return
+    int dashes = dashCounter(original, str);
+    if (dashes >= (int) floating.size()){
+      for (int i = 0; i < (int) floating.size(); i++){
+        str += (floating[i]);
+        std::vector<char> floating_cpy(floating);
+        floating_cpy.erase(floating_cpy.begin() + i);
+        helper(words, dict, original, str, floating_cpy, idx + 1);
+
+        // "undo"
+        str.erase(str.size()-1, str.size());
+      }
+    }
+    if (dashes > (int) floating.size()){
+      for (char i = 'a'; i <= 'z'; i++){
+        // loop through alphabet
+        // substitue temp (not reference!) variable
+        // recurse
+        str += i;
+        helper(words, dict, original, str, floating, idx + 1);
+
+        // "undo"
+        str.erase(str.size()-1, str.size());
+      }
+    }
+  }
+  else{
+		// if not "-",
+		// add the char at the index (that is already given)
+		// go to next index of in
+    helper(words, dict, original, str + original[idx], floating, idx + 1);
+  }
+}
+
+
+// helper()'s helper function
+// calulates the number of dashes remaining
+int dashCounter(const std::string& original, std::string str)
+{
+  int cnt = 0;
+  for (unsigned int i = str.size(); i < original.size(); i++){
+    if (original[i] == '-'){
+      cnt += 1;
+    }
+  }
+  return cnt;
+}
+
